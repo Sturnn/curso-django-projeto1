5e6e5919a602321cc348e6cc157173f88a6d3e4d
Gabriel de Souza <desouzalp007@gmail.com>
2024-09-16 20:11:57 -0300

    search bar added

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Gabriel de Souza'})
